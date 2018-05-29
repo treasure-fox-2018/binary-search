ab0db8cbf87c9d6e6cf2610c4cef5f6f7713a75d
Your Name <fahrisyany@gmail.com>
2018-05-29 20:37:43 +0700

    finished challenge

--- binary.js
+++ binary.js
@@ -1,20 +1,61 @@
 'use strict'
 
+
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+
+
+  }
+  // console.log(arr)
+
+
   return arr
+
 }
 
-function binarySearch (search, array) {
+
+
+
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+
+
+  var guess,
+    min = 0,
+    max = array.length - 1;
+
+  while (min <= max) {
+    root = Math.floor((min + max) / 2);
+    if (array[root] === search)
+      return root;
+    else if (array[root] < search)
+      min = root + 1;
+    else
+      max = root - 1;
+  }
+
+  return -1;
 }
+
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+// [ 8, 10, 10, 18, 22, 22, 32, 40, 90 ]
+// [ 3, 21, 31, 53, 53, 55, 77, 85, 89 ]
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
@@ -28,3 +69,4 @@ console.log(binary_search(2, arrayGanjilSorted))
 module.exports = {
   binary_search
 }
+
