683746289e767249412305703e4a338c88e2e10a
fajartc02 <fajartc02@gmail.com>
2018-05-29 20:51:44 +0700

    submit binarySearch

--- binary.js
+++ binary.js
@@ -5,21 +5,63 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (var a = 0; a < arr.length; a++) {
+    for (var b = a+1; b < arr.length; b++) {
+      if (arr[b] < arr[a]) {
+        var contain = arr[a]
+        arr[a] = arr[b]
+        arr[b] = contain
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+  // console.log(search)
+  var index = (array.length - 1) / 2
+  var spliceArrLow = array.slice(0, index)
+  var spliceArrHigh = array.slice(index, array.length-1)
+  console.log(spliceArrLow, 'high : ', spliceArrHigh , search)
+  if (search < array[index]) {
+    for (var a = 0; a < spliceArrLow.length; a++) {
+      var idx;
+      if (search === spliceArrLow[a]) {
+        return a
+      }
+    }
+    if (idx === undefined) {
+      return -1
+    }
+  } else if (search > array[index]) {
+    for (var b = 0; b < spliceArrHigh.length; b++) {
+      var idx;
+      if (search === spliceArrHigh[b]) {
+        return idx = b + spliceArrLow.length
+      }
+    }
+    if (idx === undefined) {
+      return -1
+    }
+    
+  } else if (search === array[index]){
+    for (var c = 0; c < spliceArrLow.length; c++) {
+      var idx;
+      if (search === spliceArrLow[c]) {
+        return idx = c
+      }
+    }
+  }
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
-// Driver code
+// // Driver code
 console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(12, arrayGenapSorted))
+console.log(binary_search(32, arrayGenapSorted))
 
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
