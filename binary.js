394a5c91eef58190d07ba3de97fbf317eb96bba8
Rhesa Utomo <venderblitz@gmail.com>
2018-05-30 01:21:43 +0700

    finished

--- binary.js
+++ binary.js
@@ -1,52 +1,62 @@
-'use strict';
+'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // insertion sort
-
+  // Your sorting code
   for (var i = 1; i < arr.length; i++) {
-    var isi = arr[i];
+    // mark 1st element as sorted -> start from index 1 not 0
+    // 'extract' element X
+    var extracted = arr[i];
 
-    for (var j = i - 1; arr[j] > isi; j--) {
-      arr[j + 1] = arr[j];
+    // for j = lastSortedIndex to 0 && if current element j > X
+    for (var j = i-1; j > -1 && arr[j] > extracted; j--) {
+      // move sorted element to the right by 1
+      arr[j+1] = arr[j];
     }
-    arr[j + 1] = isi;
-  }
-  return arr;
 
+    // insert X here
+    arr[j+1] = extracted;
+  }
+  return arr
 }
 
-function binary_search(search, array) {
-  var min = 0;
-  var max = array.length - 1;
+function binarySearch (search, array) {
+  // Your searching code
+  var leftIndex = 0;
+  var rightIndex = array.length - 1;
 
-  while (min <= max) {
-    var mid = Math.floor((min + max) / 2);
-    if (array[mid] === search) {
-      return mid;
-    } else if (array[mid] < search) {
-      min = mid + 1;
+  while (leftIndex <= rightIndex) {
+    var midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (array[midIndex] === search) {
+      return midIndex;
+    } else if (search < array[midIndex]) {
+      rightIndex = midIndex - 1;
     } else {
-      max = mid - 1;
+      leftIndex = midIndex + 1;
     }
   }
 
   return -1;
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap);
-var arrayGanjilSorted = ownSort(testArrayGanjil);
-// Driver code
-console.log(binary_search(8, arrayGenapSorted));
-console.log(binary_search(10, arrayGenapSorted));
-console.log(binary_search(33, arrayGenapSorted));
+var arrayGenapSorted = ownSort(testArrayGenap)
+var arrayGanjilSorted = ownSort(testArrayGanjil)
 
-console.log(binary_search(53, arrayGanjilSorted));
-console.log(binary_search(3, arrayGanjilSorted));
-console.log(binary_search(2, arrayGanjilSorted));
+console.log(arrayGenapSorted);
+console.log(arrayGanjilSorted);
+
+// Driver code
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
+
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
 module.exports = {
-  binary_search,
-};
+  binarySearch
+}
