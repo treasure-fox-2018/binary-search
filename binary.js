d2de724c486298420b5f516a5a775e175c8dadbc
dawimuhammad <muhamadhaddawi@gmail.com>
2018-05-30 00:07:22 +0700

    add binary search by done

--- binary.js
+++ binary.js
@@ -1,52 +1,64 @@
 'use strict';
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
+var testCaseArray = [27, 35, 16, 7, 2, 9, 43, 57, 1, 98, 23, 54];
 
-function ownSort(arr) {
-  // insertion sort
+/**
+ * @function insertionSort
+ * melaksanakan pengurutan menggunakan metode insertion
+ * @param {array} nums kumpulan angka acak dalam bentuk array
+ * @returns {array} mengembalikan array yang telah diurutkan
+ */
 
-  for (var i = 1; i < arr.length; i++) {
-    var isi = arr[i];
-
-    for (var j = i - 1; arr[j] > isi; j--) {
-      arr[j + 1] = arr[j];
+function insertionSort(nums) {
+    for (var i = 0; i < nums.length; i++) {
+        // mark firstElement as base
+        let firstElement = nums[i];
+        // count backwards to until find the lower one and move the rest one index forward
+        for (var j = i - 1; nums[j] > firstElement; j--) {
+            nums[j+1] = nums[j];
+        }
+        nums[j+1] = firstElement;
     }
-    arr[j + 1] = isi;
+    return nums;
+}
+
+/**
+ * @function binary_search
+ * mencari dengan menyisir angka tengah dari setiap pencarian
+ * @param {integer} input angka integer yang akan dicari and @param {array} testCase kumpulan angka acak dalam bentuk array
+ * @returns {boolean} mengembalikan true apabila terdapat angka yang dicari pada array dan mengembalikan -1 apabila tidak ditemukan
+ */
+
+function binary_search(input, testCase) {
+  let arrayLength = testCase.length;
+  let middle = Math.floor(arrayLength/2);
+  let sortedArray = insertionSort(testCase);
+
+  if (testCase[middle] === input) {
+      return true;
+  } else if (testCase[middle] > input) {
+      return binary_search(input, testCase.splice(middle, arrayLength-1));
+  } else if (testCase[middle] < input) {
+      return binary_search(input, testCase.splice(0, middle));
+  } else {
+      return -1;
   }
-  return arr;
 
 }
 
-function binary_search(search, array) {
-  var min = 0;
-  var max = array.length - 1;
 
-  while (min <= max) {
-    var mid = Math.floor((min + max) / 2);
-    if (array[mid] === search) {
-      return mid;
-    } else if (array[mid] < search) {
-      min = mid + 1;
-    } else {
-      max = mid - 1;
-    }
-  }
-
-  return -1;
-}
-
-var arrayGenapSorted = ownSort(testArrayGenap);
-var arrayGanjilSorted = ownSort(testArrayGanjil);
-// Driver code
-console.log(binary_search(8, arrayGenapSorted));
-console.log(binary_search(10, arrayGenapSorted));
-console.log(binary_search(33, arrayGenapSorted));
-
-console.log(binary_search(53, arrayGanjilSorted));
-console.log(binary_search(3, arrayGanjilSorted));
-console.log(binary_search(2, arrayGanjilSorted));
+//TEST CASE
+console.log(binary_search(59, testCaseArray));
 
 module.exports = {
-  binary_search,
+    binary_search,
 };
+
+
+
+
+
+
+
+
+//
