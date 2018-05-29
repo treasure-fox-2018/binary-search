fdf17366d91d9122de12356b114d1efcf8f62fae
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-29 20:45:26 +0700

    add binary.js

--- binary.js
+++ binary.js
@@ -4,27 +4,44 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i+1; j < arr.length; j++) {
+      if(arr[i] > arr[j]) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  
+  return arr;
 }
 
 function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+  var status = false;
+  
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] == search) {
+      status = true;
+      return i;
+    }
+  }
+  if(status == false) {
+    return -1;
+  }
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted));
+console.log(binarySearch(10, arrayGenapSorted));
+console.log(binarySearch(33, arrayGenapSorted));
+console.log(binarySearch(53, arrayGanjilSorted));
+console.log(binarySearch(3, arrayGanjilSorted));
+console.log(binarySearch(2, arrayGanjilSorted));
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+// module.exports = {
+//   binary_search
+// }
